351e72cfc239b80f4349bdcc7c31b36cfe83f22d
Tom Pain <hello@tompa.in>
2021-11-28 17:42:01 +0000

    Staff auth page added
    type: feat
    
    Restricted a view to admin and staff users only

--- blog/core/views.py
+++ blog/core/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from core.models import Blog
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 
 def listing(request):
     data = {
@@ -47,3 +48,7 @@ def user_info(request):
 @login_required
 def private_place(request):
     return HttpResponse("Shhh, members only!", content_type="text/plain")
+
+@user_passes_test(lambda user: user.is_staff)
+def staff_place(request):
+    return HttpResponse("Employees must wash their hands", content_type="text/plain")
